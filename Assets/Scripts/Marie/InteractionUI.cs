339296cb18aa66ff61620a258f2d96931d12bf3b
Gregory Dearham <125159238+Do0vm@users.noreply.github.com>
2024-11-06 15:49:09 +0100

    BugFix

--- Assets/Scripts/Marie/InteractionUI.cs
+++ Assets/Scripts/Marie/InteractionUI.cs
@@ -1,29 +1,76 @@
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class InteractionUI : MonoBehaviour
 {
+    private static InteractionUI _instance;
+    public static InteractionUI Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<InteractionUI>();
+                if (_instance == null)
+                {
+                    GameObject go = new GameObject("InteractionUI");
+                    go.AddComponent<Canvas>();
+                    go.AddComponent<CanvasScaler>();
+                    go.AddComponent<GraphicRaycaster>();
+                    _instance = go.AddComponent<InteractionUI>();
+                }
+            }
+            return _instance;
+        }
+    }
+
     public TextMeshProUGUI interactionText;
 
-    public static InteractionUI Instance;
-    // Start is called before the first frame update
-    void Awake()
+    private void Start()
     {
-        Instance = this;
+        // Ensure the InteractionUI GameObject has a Canvas, CanvasScaler, and GraphicRaycaster component
+        Canvas canvas = GetComponent<Canvas>();
+        if (canvas == null)
+        {
+            canvas = gameObject.AddComponent<Canvas>();
+            canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+        }
 
+        CanvasScaler canvasScaler = GetComponent<CanvasScaler>();
+        if (canvasScaler == null)
+        {
+            canvasScaler = gameObject.AddComponent<CanvasScaler>();
+            canvasScaler.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
+            canvasScaler.referenceResolution = new Vector2(1920, 1080);
+        }
+
+        GraphicRaycaster graphicRaycaster = GetComponent<GraphicRaycaster>();
+        if (graphicRaycaster == null)
+        {
+            gameObject.AddComponent<GraphicRaycaster>();
+        }
+
+        // Get the TextMeshProUGUI component
+        interactionText = GetComponentInChildren<TextMeshProUGUI>();
+        if (interactionText == null)
+        {
+            GameObject textGO = new GameObject("InteractionText");
+            textGO.transform.SetParent(transform, false);
+            interactionText = textGO.AddComponent<TextMeshProUGUI>();
+            interactionText.alignment = TextAlignmentOptions.Center;
+            interactionText.fontSize = 24;
+        }
     }
 
-    // Update is called once per frame
-    void showInteraction()
+    public void ShowInteraction(string text)
     {
-
+        interactionText.gameObject.SetActive(true);
+        interactionText.text = text;
     }
 
-
-    void hideInteraction()
+    public void HideInteraction()
     {
-
+        interactionText.gameObject.SetActive(false);
     }
 }
--- Assets/Scripts/Marie/Lever.cs
+++ Assets/Scripts/Marie/Lever.cs
@@ -1,58 +1,111 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
-using UnityEditor;
 using UnityEngine;
 
 public class Lever : MonoBehaviour
 {
     [SerializeField] private Sprite switchLeft;
     [SerializeField] private Sprite switchRight;
-    private bool leverActive;
-    private bool useLever;
-
-   void Update()
+    private bool leverActive;      // True when player is in range to use the lever
+    private bool handlePlaced;     // True when the lever handle is placed
+    private bool useLever;         // Tracks the lever's current state (on/off)
+    public GameObject Bridge;
+    void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E) && PlayerInventory.Instance.IsInInventory("LEVERHANDLE"))        //Input to handle the interaction with the handle
+        if ((leverActive && Input.GetKeyDown(KeyCode.E) && PlayerInventory.Instance.IsInInventory("LEVERHANDLE")) || (leverActive && Input.GetKeyDown(KeyCode.E) && handlePlaced))
         {
-            useLever = true;
+            if (!handlePlaced)
+            {
+                // Place the handle
+                handlePlaced = true;
+                GetComponent<SpriteRenderer>().sprite = switchLeft;
+                InteractionUI.Instance.ShowInteraction("Lever handle placed. You can now use the lever.");
+                PlayerInventory.Instance.RemoveItemFromInventory("LEVERHANDLE");
+            }
+            else
+            {
+
+                Bridge.GetComponent<Animator>().SetTrigger("Down");
+                // Toggle lever state after handle is placed
+                useLever = !useLever;
+                GetComponent<SpriteRenderer>().sprite = useLever ? switchRight : switchLeft;
+                InteractionUI.Instance.ShowInteraction("Lever toggled to " + (useLever ? "on" : "off"));
+            }
         }
-        else { useLever = false; }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        leverActive = false;
-        Debug.Log("Trigger Zorking");
-        if (useLever) // Enables lever
+        if (collision.CompareTag("Player"))
         {
-            GetComponent<SpriteRenderer>().sprite = switchLeft;
-
-            Debug.Log("This is working");
-            leverActive = true;
-
+            leverActive = true; // Player is in range to use the lever
+            InteractionUI.Instance.ShowInteraction("Press E to use the lever");
         }
-       
-        
-        if (leverActive && useLever) //After lever enabled you can use it
-        {
-            GetComponent<SpriteRenderer>().sprite = switchRight;
-        }
-        
-
-
     }
 
-
-    private void OnTriggerExit2D(Collider2D collision) // so that you cant use it anyzhere on the map
+    private void OnTriggerExit2D(Collider2D collision)
     {
-        leverActive= false;
+        if (collision.CompareTag("Player"))
+        {
+            leverActive = false; // Player left the lever area
+            InteractionUI.Instance.HideInteraction();
+        }
     }
-
-
-
-
-
-
-
 }
+
+
+
+//using System.Collections;
+//using System.Collections.Generic;
+//using System.Runtime.CompilerServices;
+//using UnityEditor;
+//using UnityEngine;
+
+//public class Lever : MonoBehaviour
+//{
+//    [SerializeField] private Sprite switchLeft;
+//    [SerializeField] private Sprite switchRight;
+//    private bool leverActive;
+//    private bool useLever;
+
+//    void Update()
+//    {
+//        if (Input.GetKeyDown(KeyCode.E) && PlayerInventory.Instance.IsInInventory("LEVERHANDLE"))        //Input to handle the interaction with the handle
+//        {
+//            useLever = true;
+//        }
+//        else { useLever = false; }
+//    }
+
+//    private void OnTriggerEnter2D(Collider2D collision)
+//    {
+//        leverActive = false;
+//        Debug.Log("Trigger Zorking");
+//        if (useLever) // Enables lever
+//        {
+//            GetComponent<SpriteRenderer>().sprite = switchLeft;
+
+//            Debug.Log("This is working");
+//            leverActive = true;
+
+//        }
+
+
+//        if (leverActive && useLever) //After lever enabled you can use it
+//        {
+//            GetComponent<SpriteRenderer>().sprite = switchRight;
+//        }
+
+
+
+//    }
+
+
+//    private void OnTriggerExit2D(Collider2D collision) // so that you cant use it anyzhere on the map
+//    {
+//        leverActive = false;
+//    }
+
+
+
+
